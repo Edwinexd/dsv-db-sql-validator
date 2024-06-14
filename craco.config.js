f29415a56e835ba90200fd7fa5a2cdf72d9a0f09
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-06-14 20:37:37 +0200

    Add sql.js

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    webpack: {
+      configure: (webpackConfig) => {
+        // Needed for sql.js
+        webpackConfig.resolve.fallback = {
+          fs: require.resolve("browserify-fs"),
+          stream: require.resolve('stream-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          vm: false,
+        };
+  
+        // Add the 'module' configuration for handling .wasm files
+        webpackConfig.module.rules.push({
+          test: /\.wasm$/,
+          type: 'javascript/auto',
+        });
+  
+        return webpackConfig;
+      },
+    },
+  };
+  
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import initSqlJs from "sql.js";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+// import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
+
 function App() {
+  const [database, setDatabase] = useState<initSqlJs.Database>();
+
+  const initDb = useCallback(async () => {
+    const SQL = await initSqlJs(
+      {
+        locateFile: (file) => `https://sql.js.org/dist/${file}`,
+      }
+    );
+    const db = new SQL.Database();
+    setDatabase(db);
+  }, []);
+
+  useEffect(() => {
+    initDb();
+  }, [initDb]);
+
+  useEffect(() => {
+    if (!database) {
+      return;
+    }
+    database.run('CREATE TABLE test (col1, col2);');
+    database.run('INSERT INTO test VALUES (?, ?);', ['Hello', 'World']);
+    const res = database.exec('SELECT * FROM test');
+    console.log(res);
+  }, [database]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to relaod.
         </p>
         <a
           className="App-link"
