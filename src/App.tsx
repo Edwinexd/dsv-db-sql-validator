7262099a97ccca910634992aec79a306a0f88c5a
Adrian <151030974+Qp0p@users.noreply.github.com>
2024-06-15 15:31:00 +0200

    Add tailwind and syntax highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import Editor from 'react-simple-code-editor';
 import './App.css';
+import logo from './logo.svg';
 
+import Prism from 'prismjs';
+// @ts-ignore
+import { highlight, languages } from 'prismjs/components/prism-core';
 import initSqlJs from "sql.js";
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-sql';
+import 'prismjs/themes/prism.css';
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
-// import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
+
+
+//eslint-disable-next-line import/no-webpack-loader-syntax
+//import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
 
 function App() {
   const [database, setDatabase] = useState<initSqlJs.Database>();
@@ -19,6 +28,25 @@ function App() {
     const db = new SQL.Database();
     setDatabase(db);
   }, []);
+
+  const Component = () => {
+    
+    const [code, setCode] = React.useState(
+      `function add(a, b) {\n  return a + b;\n}`
+    );
+    return (
+      <Editor
+        value={code}
+        onValueChange={code => setCode(code)}
+        highlight={code => highlight(code, languages.sql)}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+        }}
+      />
+    );
+  };
 
   useEffect(() => {
     initDb();
@@ -38,8 +66,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Component></Component>
         <p>
-          Edit <code>src/App.tsx</code> and save to relaod.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
