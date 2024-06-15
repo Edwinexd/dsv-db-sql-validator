be3bf3dacef056508752a9c118367cfe2751f33e
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-06-16 00:33:57 +0200

    Rewrite comparisons to used hashed sets

--- src/utils.ts
+++ src/utils.ts
@@ -1,69 +1,167 @@
 
+const cyrb53 = (str: string, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+class HashedRow {
+    private values: (number | string | Uint8Array | null)[] = [];
+    private hash: number;
+
+    constructor(data: (number | string | Uint8Array | null)[]) {
+        this.values = data;
+
+        this.hash = 17;
+        for (let i = 0; i < data.length; i++) {
+            if (typeof data[i] === 'string') {
+                this.hash *= cyrb53(data[i] as string);
+            } else if (typeof data[i] === 'number') {
+                this.hash *=  data[i] as number;
+            } else if (data[i] === null) {
+                this.hash *= 17;
+            } else {
+                for (let j = 0; j < (data[i] as Uint8Array).length; j++) {
+                    this.hash *= (data[i] as Uint8Array)[j];
+                }
+            }
+        }
+    }
+
+    equals(other: HashedRow) {
+        if (this.values.length !== other.values.length) {
+            return false;
+        }
+
+        if (this.hash !== other.hash) {
+            return false;
+        }
+
+        // Try match all values in this set to the other set
+        return this.backtrackCompare(0, new Array<boolean>(other.values.length).fill(false), other.values);
+    }
+
+    private backtrackCompare(index: number, used: boolean[], otherValues: (number | string | Uint8Array | null)[]): boolean {
+        if (index === this.values.length) {
+            return true;
+        }
+
+        for (let i = 0; i < otherValues.length; i++) {
+            if (!used[i] && this.valueEquals(this.values[index], otherValues[i])) {
+                used[i] = true;
+                if (this.backtrackCompare(index + 1, used, otherValues)) {
+                    return true;
+                }
+                used[i] = false;
+            }
+        }
+
+        return false;
+    }
+
+    private valueEquals(value1: number | string | Uint8Array | null, value2: number | string | Uint8Array | null): boolean {
+        if (value1 === value2) {
+            return true;
+        }
+
+        if (value1 instanceof Uint8Array && value2 instanceof Uint8Array) {
+            if (value1.length !== value2.length) {
+                return false;
+            }
+            for (let i = 0; i < value1.length; i++) {
+                if (value1[i] !== value2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    getHash() {
+        return this.hash;
+    }
+}
+
+class RowHashSet {
+    // array of arrays of hashed rows
+    private buckets: HashedRow[][] = [];
+
+    constructor(size: number = 100) {
+        this.buckets = new Array(size).fill([]).map(() => []);
+    }
+
+    add(row: HashedRow) {
+        const bucket = this.getBucket(row);
+        bucket.push(row);
+    }
+
+    remove(row: HashedRow) {
+        const bucket = this.getBucket(row);
+        const index = bucket.findIndex(r => r.equals(row));
+        if (index !== -1) {
+            bucket.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    contains(row: HashedRow) {
+        const bucket = this.getBucket(row);
+        return bucket.some(r => r.equals(row));
+    }
+
+    private getBucket(row: HashedRow): HashedRow[] {
+        let hash = row.getHash();
+        if (hash < 0) {
+            hash = -hash;
+        }
+        return this.buckets[hash % this.buckets.length];
+    }
+
+    isEmpty() {
+        return this.buckets.every(bucket => bucket.length === 0);
+    }
+}
+
 interface Result {
     columns: string[];
     data: (number | string | Uint8Array | null)[][];
 }
-
+    
 
 export function isCorrectResult(expected: Result, actual: Result) {
-    // Results may have a different ordering of both columns and data
-    // column names may differ but it should still be considered correct
-    // if the data is the same
-
     // Check if the columns are the same
     if (expected.columns.length !== actual.columns.length) {
         return false;
     }
 
-    let globalCandidates: { [key: number]: number[] } = {};
-    // For each column in expected, try to see if it exists in actual
-    for (let i = 0; i < expected.columns.length; i++) {
-        let candidates = [];
-        for (let j = 0; j < actual.columns.length; j++) {
-            const dataInExpected = expected.data.map(row => row[i]);
-            const dataInActual = actual.data.map(row => row[j]);
+    // Convert rows to hashed rows
+    const expectedRows = expected.data.map(row => new HashedRow(row));
+    const actualRows = actual.data.map(row => new HashedRow(row));
 
-            if (dataInExpected.length === dataInActual.length) {
-                // Make copy of both data arrays
-                dataInExpected.sort();
-                dataInActual.sort();
-                // Compare the data
-                let failed = false;
-                for (let k = 0; k < dataInExpected.length; k++) {
-                    if (dataInExpected[k] !== dataInActual[k]) {
-                        console.log('Data mismatch', dataInExpected[k], dataInActual[k])
-                        failed = true;
-                        break;
-                    }
-                }
-                if (!failed) {
-                    candidates.push(j);
-                }
-            }
-        }
-        if (candidates.length === 0) {
-            return false;
-        }
-        globalCandidates[i] = candidates;
+    // put actualRows in hashset for fast removal
+    const actualRowsSet = new RowHashSet(actualRows.length);
+
+    for (const row of actualRows) {
+        actualRowsSet.add(row);
     }
 
-    // Try matching globalCandidates so each key has a unique value
-    const usedColumns = new Set<number>();
-    const matchColumns = (index: number): boolean => {
-        if (index === expected.columns.length) {
-            return true;
+    for (const row of expectedRows) {
+        if (!actualRowsSet.remove(row)) {
+            return false;
         }
-        for (const candidate of globalCandidates[index]) {
-            if (!usedColumns.has(candidate)) {
-                usedColumns.add(candidate);
-                if (matchColumns(index + 1)) {
-                    return true;
-                }
-                usedColumns.delete(candidate);
-            }
-        }
-        return false;
-    };
+    }
 
-    return matchColumns(0);
+    return actualRowsSet.isEmpty();
 }
